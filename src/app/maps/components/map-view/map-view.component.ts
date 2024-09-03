7c0c1f4e3034e228ceae6a9935363b0287af8560
jere <jere77lagos@gmail.com>
2024-09-03 19:11:28 +0200

    implement map, popups and logo

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,18 +1,41 @@
+import { Map, Popup, Marker } from 'mapbox-gl';
 import { PlacesService } from './../../services/places.service';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild,  } from '@angular/core';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
 
-  constructor( private PlacesService: PlacesService) {
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
 
+  constructor( private placesService: PlacesService) {}
+
+  ngAfterViewInit(): void {
+
+    if ( !this.placesService.useLocation ) throw Error('There is not placesService.userLocation')
+
+    const map = new Map({
+      container: this.mapDivElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
+      center: this.placesService.useLocation, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+    });
+
+    const popup = new Popup()
+      .setHTML(`
+        <h6>Here I am</h6>
+        <span>In this part of the World.</span>
+      `)
+
+    new Marker({ color: 'red' })
+        .setLngLat( this.placesService.useLocation )
+        .setPopup( popup )
+        .addTo( map )
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVyZW1pYXNsciIsImEiOiJjbTBtbHR0NmgwNDU4Mm1zZ2FzdTR5M3MxIn0.M4CinAqrKoeErhwmtLe5aw';
+
 if( !navigator.geolocation ) {
   alert('Browser does not support geolocation')
   throw new Error('Browser does not support geolocation')
