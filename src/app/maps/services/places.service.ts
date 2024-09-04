aceecdbd9817d3ecbf4e489e8dd846de23602e02
jere <jere77lagos@gmail.com>
2024-09-04 12:55:38 +0200

    implement search results funcionality

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,15 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public useLocation?: [number, number]
+  public isLoadingPlaces: boolean = false
+  public places: Feature[] = []
+
 
   get isUserLocationReady(): boolean {
     return !!this.useLocation
   }
 
-  constructor() {
+  constructor( private http: HttpClient ) {
     this.getUserLocation()
    }
 
@@ -30,4 +35,17 @@ export class PlacesService {
       )
     })
   }
+
+  getPlacesByQuery( query: string = '' ) {
+
+    this.isLoadingPlaces = true
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/search/geocode/v6/forward?q=${query}&limit=5&proximity=-73.96551635400584%2C40.78159954735071&language=es&access_token=pk.eyJ1IjoiamVyZW1pYXNsciIsImEiOiJjbTBtbHR0NmgwNDU4Mm1zZ2FzdTR5M3MxIn0.M4CinAqrKoeErhwmtLe5aw`)
+      .subscribe(resp => {
+        console.log(resp.features)
+        this.isLoadingPlaces = false
+        this.places = resp.features
+      })
+  }
+
 }
